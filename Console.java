259aa3d4fa3c23bd3e70b74b5ea2ade46186f26a
joao-vitor8217 <148113167+joao-vitor8217@users.noreply.github.com>
2023-10-25 09:34:12 -0300

    Add files via upload

---
+++ Console.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Console {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite seu nome: ");
+		String nome = entrada.nextLine();
+		System.out.print("Agora digite seu sobrenome: ");
+		String sobrenome = entrada.nextLine();
+		System.out.print("Agora fale sua idade: ");
+		int idade = entrada.nextInt();
+		System.out.println("Seu nome é: " + nome);
+		System.out.println("Seu sobrenome é: " + sobrenome);
+		System.out.println("Sua idade é de: " + idade + " anos");
+		
+	}
+
+}
