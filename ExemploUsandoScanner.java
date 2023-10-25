67489dc35a83edf369c76eb7b58bda1a60c26aaf
joao-vitor8217 <148113167+joao-vitor8217@users.noreply.github.com>
2023-10-25 09:42:42 -0300

    Update ExemploUsandoScanner.java

--- ExemploUsandoScanner.java
+++ ExemploUsandoScanner.java
@@ -22,3 +22,30 @@ public class Console {
 	}
 
 }
+
+//Posso fazer de outra maneira tbm usando printf
+
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Console {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite seu nome: ");
+		String nome = entrada.nextLine();
+		System.out.print("Agora digite seu sobrenome: ");
+		String sobrenome = entrada.nextLine();
+		System.out.print("Agora fale sua idade: ");
+		int idade = entrada.nextInt();
+		System.out.printf("Seu nome é %s %s e tem %d anos", nome,
+				                                            sobrenome, idade);
+		
+		entrada.close();
+		
+	}
+
+}
+
